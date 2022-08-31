5c80e06bf67af41b9eabbf28803183d007f8be63
Jo Lingenfelter <joanna.lingenfelter@willowtreeapps.com>
2022-08-31 11:04:56 -0400

    Add XcodeProject

---
+++ TestGraphQLServer-Apollo/TestGraphQLServer-Apollo/TestGraphQLServer_ApolloApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGraphQLServer_ApolloApp.swift
+//  TestGraphQLServer-Apollo
+//
+//  Created by Joanna Lingenfelter on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct TestGraphQLServer_ApolloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
